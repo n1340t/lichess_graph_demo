77a7a96dfe3adbf651385056816d330d01ef9f1f
btran <nguyenbao1003@outlook.com>
2023-05-14 22:52:09 -0400

    Add basic UI design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import {
+  MDBBtn,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBInputGroup,
+  MDBRow,
+  MDBCheckbox
+} from 'mdb-react-ui-kit';
+import { Fragment } from 'react';
+
 function App() {
-  return <h1>My React and TypeScript App!</h1>;
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol>
+          <h1>Lichess Neo4J Demo</h1>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol>
+          <form>
+            <MDBInputGroup
+              noBorder
+              size='lg'
+              textBefore={
+                <Fragment>
+                  <MDBIcon fas icon='chess-king' size='2x' />
+                  <MDBIcon fas icon='hashtag' size='2x' />
+                </Fragment>
+              }
+            >
+              <input
+                className='form-control'
+                type='text'
+                placeholder='Enter lichess username'
+              />
+            </MDBInputGroup>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
 
 export default App;
